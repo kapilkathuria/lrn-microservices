bc3ac2688a5c5beac229a267c35b1fc011bf815a
kapil <kapil.kathuria@gmail.com>
2020-08-02 17:00:06 +0530

    submit new post workign from client react app

--- posts/index.js
+++ posts/index.js
@@ -3,9 +3,11 @@
 const express = require('express');
 const  bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
