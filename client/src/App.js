82cc3da023f9436536c0f55f153fba35bc1298b7
kapil <kapil.kathuria@gmail.com>
2020-08-03 15:25:17 +0530

    posts cards created. now posts can be seens in different cardds on page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
-    return <div className="container">
+    return (
+    <div className="container">
         <h1>Create Post</h1>
-        <PostCreate></PostCreate>
-    </div>
+        <PostCreate />
+        <hr></hr>
+        <h1>Posts</h1>
+        <PostList></PostList>
+    </div>)
 }
